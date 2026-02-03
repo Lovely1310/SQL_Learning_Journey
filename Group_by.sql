995e5c68026843e42e077b3b0ba854cefb8010b9
Monika Saini <monika9899803392@gmail.com>
2026-02-03 22:43:47 +0530

    Group_by & Having_Clause.sql
    The GROUP BY clause is used to group rows that have the same values in specified columns. It is typically used with aggregate functions like COUNT , SUM ,
    AVG , MIN , or MAX .
    
    The HAVING clause is used to filter groups after aggregation — similar to how WHERE filters individual rows.

---
+++ Group_by.sql
@@ -0,0 +1,26 @@
+CREATE TABLE admin_users (
+id INT PRIMARY KEY,
+ name VARCHAR(100),
+ email VARCHAR(100),
+ gender ENUM('Male', 'Female', 'Other'),
+ date_of_birth DATE,
+ salary INT
+);
+
+INSERT INTO admin_users (id, name, email, gender, date_of_birth, salary) VALUES
+(101, 'Anil Kumar', 'anil@example.com', 'Male', '1985-04-12', 60000),
+(102, 'Pooja Sharma', 'pooja@example.com', 'Female', '1992-09-20', 58000),
+(103, 'Rakesh Yadav', 'rakesh@example.com', 'Male', '1989-11-05', 54000),
+(104, 'Fatima Begum', 'fatima@example.com', 'Female', '1990-06-30', 62000);
+
+select  gender as Gender, avg(salary) as AVG_Salary,count(*) as Count 
+from admin_users group by gender
+having avg(salary)<61000 and count(*) < 19;
+
+-- where is used before group by , having is used after group by because having aggregates function
+
+SELECT gender as Gender , count(*) as Total_users from admin_users 
+group by gender with ROLLUP;
+
+select  gender as Gender, avg(salary) as AVG_Salary,count(*) as Count 
+from admin_users group by gender with ROLLUP;
